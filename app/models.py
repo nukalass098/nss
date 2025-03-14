8dbbc9949a005a76d59e7887dc5e93299da77753
nukalass098 <nukalass098@gmail.com>
2025-03-14 20:29:23 +0530

    Add files via upload

--- app/models.py
+++ app/models.py
@@ -42,3 +42,11 @@ class Submit(models.Model):
     date = models.DateField(null=False)
     def __str__(self):
         return self.name
+
+class Save(models.Model):
+    name = models.CharField(max_length=20, null=False)
+    date = models.DateField(null=False)
+    bill = models.IntegerField(null=False)
+    balance = models.IntegerField(null=False)
+    def __str__(self):
+        return self.name
